f1f32ce130b6a156c2b8c6fccbf01499cf4efa5c
Dennis Demir <dennisjdemir@gmail.com>
2024-03-05 22:15:34 -0500

    add class to body to toggle dark mode on whole screen

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 const darkModeToggle = document.getElementById('darkModeToggle');
 const container = document.querySelector('.container');
+const body = document.querySelector('body')
 
 darkModeToggle.addEventListener('change', () => {
     if (darkModeToggle.checked){
         container.classList.add('dark-mode')
+        body.classList.add('dark-mode')
     } else {
         container.classList.remove('dark-mode');
     }
